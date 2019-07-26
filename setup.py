3ae1cd2bac7bcf3c1ca0f7e0ec64a74aa56922c0
David Tew <davetew@alum.mit.edu>
2019-07-26 11:43:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='Energy-Market-Tools',
+   name='EnergyMarketTools',
    version='0.1',
    description='A collection of energy-market tools that leverate the US DOE EIA API',
    author='David Tew',
    author_email='davetew@alum.mit.edu',
-   packages=['Energy-Market-Tools'],  #same as name
+   packages=['EnergyMarketTools'],  #same as name
    install_requires=['numpy', 'matplotlib','pandas','requests'], #external packages as dependencies
 )
