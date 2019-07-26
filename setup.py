f2314c80cbee2d621537f00dfda4706d54803b26
David Tew <davetew@alum.mit.edu>
2019-07-26 10:54:18 -0400

    Create setup.py
    Initial draft of setup.py.  Created with the intention of enabling the modules importation into Google colab.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Energy-Market-Tools',
+   version='0.1',
+   description='A collection of energy-market tools that leverate the US DOE EIA API',
+   author='David Tew',
+   author_email='davetew@alum.mit.edu',
+   packages=['Energy-Market-Tools'],  #same as name
+   install_requires=['bar', 'greek'], #external packages as dependencies
+)
