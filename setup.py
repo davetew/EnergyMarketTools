367590c51a6328f2fae5f8fc61c1d4599bebd1de
David Tew <davetew@alum.mit.edu>
2019-07-30 20:54:29 -0400

    Update setup.py
    Add the fredapi external module

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
    name='EnergyMarketTools',
    version='0.1',
-   description='A collection of energy-market tools that leverate the US DOE EIA API',
+   description='A collection of energy-market tools that leverate the US DOE EIA & FRED APIs',
    author='David Tew',
    author_email='davetew@alum.mit.edu',
    packages=['EnergyMarketTools'],  #same as name
-   install_requires=['numpy', 'matplotlib','pandas','requests'], #external packages as dependencies
+   install_requires=['numpy', 'matplotlib','pandas','requests','fredapi'], #external packages as dependencies
 )
