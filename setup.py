f756469b7e6a43898c4ebcda05c4d86c1eced52d
David Tew <davetew@alum.mit.edu>
2021-01-20 11:43:39 -0500

    Update setup.py
    Automatically detect packages

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
    description='A collection of energy-market tools that leverate the US DOE EIA & FRED APIs',
    author='David Tew',
    author_email='davetew@alum.mit.edu',
-   packages=['EnergyMarketTools'],  #same as name
+   url='https://github.com/davetew/EnergyMarketTools',
+   packages=setuptools.find_packages(),  #same as name
    install_requires=['numpy', 'matplotlib','pandas','requests','fredapi'], #external packages as dependencies
 )
